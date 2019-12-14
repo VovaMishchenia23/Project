37bb841a8461f45b7da6c291511ed44c48432410
VovaMishchenia23 <Vova2004mis@gmail.com>
2019-12-14 17:12:44 +0200

    First commit

---
+++ Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProject
+{
+    [Serializable]
+    public class Student
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public List<Mark> Marks { get; set; }
+        public Student()
+        {
+           Marks= new List<Mark>();
+        }
+        public void AddMark(string subject,int raiting)
+        {
+            Mark m = new Mark();
+            m.Raiting = raiting;
+            m.Subject = subject;
+            Marks.Add(m);
+        }
+        public override string ToString()
+        {
+            string raiting = String.Empty;
+            foreach (var item in Marks)
+            {
+                raiting += Environment.NewLine +item;
+            }
+            return $"{Name} {Lastname}, {Age}, {raiting}";
+        }
+
+
+    }
+    [Serializable]
+    public class Mark
+    {
+        
+        public int Raiting { get; set; }
+        public string Subject { get; set; }
+        public override string ToString()
+        {
+            return $"{Subject}--------------{Raiting}";
+        }
+    }
+
+   
+}
